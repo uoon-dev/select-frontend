1ef4e4251bd3cbbbd0a94aa01a4e6227902a35d0
이준우 <junwoo.lee@ridi.com>
2019-08-06 12:07:46 +0900

    error page 에서 인앱 셀렉트 초기 로딩 해지해주는 부분 누락된 것 추가.
    (cherry picked from commit e128b87d552723477de1386f5d4bd601c7765ef4)
    
    # Conflicts:
    #	src/app/scenes/ErrorPage.tsx

--- src/app/scenes/ErrorPage.tsx
+++ src/app/scenes/ErrorPage.tsx
@@ -16,7 +16,18 @@ interface ErrorPageStateProps {
 type Props = ErrorPageStateProps & ReturnType<typeof mapDispatchToProps>;
 
 export class ErrorPage extends React.Component<Props> {
+<<<<<<< HEAD
   public componentWillUpdate(nextProps: Props) {
+=======
+  public componentDidMount() {
+    if (window.inApp && window.inApp.initialRendered) {
+      window.inApp.initialRendered();
+    }
+  }
+
+  public componentDidUpdate() {
+    const { responseData, requestMaintenanceData } = this.props;
+>>>>>>> e128b87... error page 에서 인앱 셀렉트 초기 로딩 해지해주는 부분 누락된 것 추가.
     if (
       !nextProps.responseData ||
       nextProps.responseData.status !== ErrorStatus.MAINTENANCE ||
