db4323f8de812270794125afe939f04620d7c33e
이준우 <junwoo.lee@ridi.com>
2019-08-22 10:56:37 +0900

    인앱 로그인용 화면 컴포넌트에서 뒤로가기 후 500ms 뒤에 새로고침 시도하도록 timer 설정.

--- src/app/scenes/InAppLoginRequired.tsx
+++ src/app/scenes/InAppLoginRequired.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 
 export const InAppLoginRequired: React.FunctionComponent = () => {
+  React.useEffect(() => {
+    window.history.back();
+    setTimeout(() => { window.location.reload(); }, 500);
+  }, []);
+
   return (
     <main className="SceneWrapper" />
   );
