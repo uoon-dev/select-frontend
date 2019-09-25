493c7f8695adf96e76e2d41b96db5f5ce0ef8ed3
JunWoo <junu.sinclair@gmail.com>
2019-09-25 17:45:15 +0900

    점검 페이지 수정.
    (cherry picked from commit d35abe0a3302c289789edc4e2249c1e2c5afc2b8)

--- src/app/components/MaintenanceContext.tsx
+++ src/app/components/MaintenanceContext.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '@ridi/rsg';
 import { ConnectedCompactPageHeader } from 'app/components/CompactPageHeader';
 import { ErrorResponseData } from 'app/services/serviceStatus';
 import * as React from 'react';
@@ -6,35 +7,52 @@ interface MaintenanceContextProps {
   responseData: ErrorResponseData;
 }
 
+const ServiceList: React.SFC<{ services: string[] }> = ({ services }) => (
+  <ul>
+    {services.map((service) => (
+      <React.Fragment key={service}>
+        <li>
+          <div>{service}</div>
+        </li>
+      </React.Fragment>
+    ))}
+  </ul>
+);
+
 export const MaintenanceContext: React.FunctionComponent<MaintenanceContextProps> = (props) => {
   const { responseData } = props;
 
   return (
     <section className="PageMaintenance">
       <ConnectedCompactPageHeader />
-      <h2 className="MaintenanceTitle">점검 안내</h2>
-      <div className="MaintenanceDescription MaintenanceDescription_Box">
-        <h3 className="MaintenanceSubTitle">점검기간</h3>
-        <p className="MaintenanceDescription_Text">{responseData.period}</p>
-        <h3 className="MaintenanceSubTitle">점검 중 이용이 제한되는 서비스</h3>
-        <ul className="MaintenanceDescription_ServiceList">
-          {responseData.unavailableService.map((service) => (
-            <li
-              className="MaintenanceDescription_ServiceItem"
-              key={service}
-            >
-              {service}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <p className="MaintenanceDescription">
-        보다 나은 서비스를 제공해 드리기 위한 시스템 점검으로,
-        이용에 불편함을 드리게 된 점 양해 부탁드립니다.<br />
-        언제나 편리하고 즐겁게 리디셀렉트를 이용하실 수 있도록
-        최선을 다하겠습니다.<br />
-        감사합니다.<br />
-      </p>
+      <main className="MaintenanceWrapper">
+        <header className="MaintenanceHeader">
+          <h1>시스템 점검 안내</h1>
+          <p>안녕하세요.</p>
+          보다 나은 서비스 제공을 위해 시스템 점검을 실시합니다.<br />
+          점검 중에는 일부 서비스 제공이 어려우니 양해 부탁드립니다.
+        </header>
+        <section className="MaintenanceContentSection">
+          <Icon name="exclamation_3" />
+          <dl>
+            <dt>점검 기간</dt>
+            <dd>
+              <ul>
+                <li>{responseData.period}</li>
+              </ul>
+            </dd>
+            <dt>점검 기간 중 이용이 제한되는 서비스</dt>
+            <dd>
+              <ServiceList services={responseData.unavailableService || []} />
+            </dd>
+          </dl>
+        </section>
+        <footer className="MaintenanceFooter">
+          언제나 편리하게 서비스를 이용하실 수 있도록 최선을 다하겠습니다.<br />
+          감사합니다.
+        </footer>
+      </main>
+
     </section>
   );
 };
