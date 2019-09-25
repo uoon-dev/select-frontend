dc1cb3a2cecf901d807adca633b4188baed207a8
JunWoo <junu.sinclair@gmail.com>
2019-09-26 02:24:10 +0900

    resolve conflict

--- src/app/scenes/ErrorPage.tsx
+++ src/app/scenes/ErrorPage.tsx
@@ -17,30 +17,16 @@ interface ErrorPageStateProps {
 type Props = ErrorPageStateProps & ReturnType<typeof mapDispatchToProps>;
 
 export class ErrorPage extends React.Component<Props> {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  public componentWillUpdate(nextProps: Props) {
-=======
-  public componentDidMount() {
-    if (window.inApp && window.inApp.initialRendered) {
-      window.inApp.initialRendered();
-    }
-  }
-
-  public componentDidUpdate() {
-=======
   private getMaintenanceData() {
->>>>>>> 1cd7539... maintenance 대응 수정.
     const { responseData, requestMaintenanceData } = this.props;
->>>>>>> e128b87... error page 에서 인앱 셀렉트 초기 로딩 해지해주는 부분 누락된 것 추가.
     if (
-      !nextProps.responseData ||
-      nextProps.responseData.status !== ErrorStatus.MAINTENANCE ||
-      (nextProps.responseData.period && nextProps.responseData.unavailableService)
+      !responseData ||
+      responseData.status !== ErrorStatus.MAINTENANCE ||
+      (responseData.period && responseData.unavailableService)
     ) {
       return;
     }
-    nextProps.requestMaintenanceData();
+    requestMaintenanceData();
   }
 
   private renderErrorContent() {
