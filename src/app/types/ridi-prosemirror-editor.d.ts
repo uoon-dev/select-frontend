29fd5ee8c721e08e5c1bf08b71ecf74bdd34c286
JunWoo <junu.sinclair@gmail.com>
2019-10-17 09:58:57 +0900

    Article Component Typing

--- src/app/types/ridi-prosemirror-editor.d.ts
+++ src/app/types/ridi-prosemirror-editor.d.ts
@@ -1,4 +1,29 @@
+
 declare module '@ridi/ridi-prosemirror-editor' {
-  export const Article: any;
-  export const ArticleEditor: any;
+  import { FunctionComponent } from 'react';
+
+  export interface TextContent {
+    type: string;
+    content: string;
+  }
+  export interface DecoratorOfContent {
+    type: string;
+  }
+  export interface AttrbuteOfContent {
+    [keyName: string]: number | string;
+  }
+
+  export interface ArticleContentJSON {
+    type: string;
+    content: Array<{
+      type: string;
+      attr?: AttrbuteOfContent;
+      marks?: DecoratorOfContent[];
+      content: TextContent[] | string;
+    }>;
+  }
+  export const Article: FunctionComponent<{
+    json: ArticleContentJSON;
+    style?: AttrbuteOfContent;
+  }>;
 }
