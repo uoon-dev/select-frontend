b155286edb77ed573682ac7e225339b77a9c2038
이준우 <junwoo.lee@ridi.com>
2019-11-04 17:05:40 +0900

    인트로 화면 불필요한 스타일 속성 분산 제거.

--- src/app/scenes/Intro.tsx
+++ src/app/scenes/Intro.tsx
@@ -2,7 +2,7 @@ import * as classNames from 'classnames';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Icon } from '@ridi/rsg';
+import { Button, Icon } from '@ridi/rsg';
 
 import { HelmetWithTitle, TitleType } from 'app/components';
 import { PageTitleText } from 'app/constants';
@@ -52,31 +52,26 @@ export const Intro: React.FunctionComponent = () => {
       )}
       <h1 className="a11y">리디셀렉트 인트로</h1>
       <section
-        className={classNames({
-          Section: true,
-          SectionMain: true,
-          active: isLoaded,
-        })}
+        className={classNames([
+          'SectionMain',
+          isLoaded && 'active',
+        ])}
       >
         <div className="SectionMain_Content">
-          <h2 className="Section_MainCopy SectionMain_MainCopy">
+          <h2 className="SectionMain_MainCopy">
             신간도 베스트셀러도<br />
             월정액으로 제한없이
           </h2>
-          <p className="Section_Description SectionMain_Description">
+          <p className="SectionMain_Description">
             {FREE_PROMOTION_MONTHS}개월 무료 후 월 6,500원
             <br />
             언제든 원클릭으로 해지
           </p>
-          <button
+          <Button
             type="button"
-            className={classNames(
-              'RUIButton',
-              'RUIButton-color-blue',
-              'RUIButton-size-large',
-              'Section_Button',
-              'SectionMain_Button',
-            )}
+            className="SectionMain_Button"
+            color="blue"
+            size="large"
             onClick={() => {
               if (isLoggedIn) {
                 window.location.replace(`${BASE_URL_STORE}/select/payments`);
@@ -90,7 +85,7 @@ export const Intro: React.FunctionComponent = () => {
               '리디셀렉트 구독하기'
             }
             <Icon name="arrow_5_right" className="RSGIcon-arrow5Right" />
-          </button>
+          </Button>
         </div>
       </section>
     </main>
