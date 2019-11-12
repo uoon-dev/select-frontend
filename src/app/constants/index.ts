4206e6b5d6f4b843071e975784ec7f666f17a19f
JunWoo <junu.sinclair@gmail.com>
2019-11-12 11:09:33 +0900

    아티클 컨텐츠 화면 url 구조 수정.

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -70,5 +70,5 @@ export enum RoutePaths {
   ARTICLE_CHANNELS = '/article/channels',
   ARTICLE_CHANNEL_DETAIL = '/article/channel/:channelId',
   ARTICLE_FAVORITE = '/article/favorite',
-  ARTICLE_CONTENT = '/article/content/:contentId',
+  ARTICLE_CONTENT = '/article/@:channelName/:contentKey',
 }
