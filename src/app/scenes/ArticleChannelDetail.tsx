bc014e5d68bf397995de3652e771b93e67fadf03
이준우 <junwoo.lee@ridi.com>
2019-11-13 14:32:02 +0900

    채널 상세페이지에 페이지네이션 추가.

--- src/app/scenes/ArticleChannelDetail.tsx
+++ src/app/scenes/ArticleChannelDetail.tsx
@@ -2,35 +2,40 @@ import { RidiSelectState } from 'app/store';
 import * as classNames from 'classnames';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, LinkProps, useParams } from 'react-router-dom';
 
 import { Actions } from 'app/services/articleChannel';
 import { getPageQuery } from 'app/services/routing/selectors';
 
-import { HelmetWithTitle } from 'app/components';
+import { HelmetWithTitle, Pagination } from 'app/components';
 import { ArticleChannelMeta } from 'app/components/ArticleChannelDetail/ArticleChannelMeta';
 import { GridArticleList } from 'app/components/GridArticleList';
+import { articleChannelToPath } from 'app/utils/toPath';
+import MediaQuery from 'react-responsive';
 
 export const ArticleChannelDetail: React.FunctionComponent = () => {
   const channelId = Number(useParams<{ channelId: string }>().channelId);
   const page = useSelector(getPageQuery);
-  const { articleChannelById } = useSelector((state: RidiSelectState) => state);
-  const { articlesById } = useSelector((state: RidiSelectState) => state);
+  const itemCountPerPage = 24;
+  const { articleChannelData, articlesById } = useSelector((state: RidiSelectState) => ({
+    articleChannelData: state.articleChannelById[channelId] ? state.articleChannelById[channelId] : undefined,
+    articlesById: state.articlesById,
+  }));
   const dispatch = useDispatch();
 
   const isFetched = () => {
-    return (channelId && articleChannelById && articleChannelById[channelId]);
+    return (channelId && articleChannelData);
   };
   const isFetchedChannelMeta = () => {
     if (!isFetched()) { return false; }
-    return articleChannelById[channelId].isMetaFetched;
+    return articleChannelData!.isMetaFetched;
   };
   const isFetchedChannelArticles = () => {
     if (!isFetched()) { return false; }
     return (
-      articleChannelById[channelId].itemListByPage &&
-      articleChannelById[channelId].itemListByPage[page] &&
-      articleChannelById[channelId].itemListByPage[page].isFetched
+      articleChannelData!.itemListByPage &&
+      articleChannelData!.itemListByPage[page] &&
+      articleChannelData!.itemListByPage[page].isFetched
     );
   };
   React.useEffect(() => {
@@ -41,36 +46,50 @@ export const ArticleChannelDetail: React.FunctionComponent = () => {
       dispatch(Actions.loadArticleChannelArticlesRequest({channelId, page}));
     }
   }, []);
-
-  return (
+  return articleChannelData && articleChannelData.channelMeta ? (
     <main
       className={classNames(
         'SceneWrapper',
       )}
     >
-      <HelmetWithTitle titleName={isFetchedChannelMeta() ? articleChannelById[channelId].channelMeta!.displayName : ''} />
+      <HelmetWithTitle titleName={isFetchedChannelMeta() ? articleChannelData.channelMeta!.displayName : ''} />
       <div className="a11y"><h1>리디셀렉트 아티클 채널</h1></div>
       {
         isFetchedChannelMeta() &&
-        <ArticleChannelMeta {...articleChannelById[channelId].channelMeta!} />
+        <ArticleChannelMeta {...articleChannelData.channelMeta!} />
       }
       <div className="Channel_ArticleList">
-        {
-          isFetchedChannelArticles() &&
+        {isFetchedChannelArticles() &&
           <GridArticleList
             pageTitleForTracking="article-channel-detail"
             uiPartTitleForTracking="article-channel-detail-articles"
             renderAuthor={false}
             articles={
-              articleChannelById[channelId]
+              articleChannelData
                 .itemListByPage[page]
                 .itemList
                 .map((id) => articlesById[id].article!)
             }
           />
-
+        }
+        {articleChannelData.itemCount &&
+          <MediaQuery maxWidth={840}>
+            {(isMobile) => (
+              <Pagination
+                currentPage={page}
+                totalPages={Math.ceil(articleChannelData.itemCount! / itemCountPerPage)}
+                isMobile={isMobile}
+                item={{
+                  el: Link,
+                  getProps: (p): LinkProps => ({
+                    to: `${articleChannelToPath({ channelId })}?page=${p}`,
+                  }),
+                }}
+              />
+            )}
+          </MediaQuery>
         }
       </div>
     </main>
-  );
+  ) : null;
 };
