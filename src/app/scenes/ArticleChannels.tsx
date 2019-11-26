8e0c60c239a12f0cadf31ea430b5ba06ce911b8f
오민형 <minhyung@ridi.com>
2019-11-26 15:23:43 +0900

    전체채널 페이지의 ArticleChannelList 컴포넌트 통합

--- src/app/scenes/ArticleChannels.tsx
+++ src/app/scenes/ArticleChannels.tsx
@@ -1,10 +1,23 @@
 import { HelmetWithTitle } from 'app/components';
-import { ArticleChannelList } from 'app/components/ArticleChannels/ArticleChannelList';
-import { PageTitleText } from 'app/constants';
+import { ArticleChannelsMeta } from 'app/components/ArticleChannels/ArticleChannelsMeta';
+import { FetchStatusFlag, PageTitleText } from 'app/constants';
+import { Actions } from 'app/services/articleChannel';
+import { getChannelList } from 'app/services/articleChannel/selectors';
+import { RidiSelectState } from 'app/store';
 import * as classNames from 'classnames';
 import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const ArticleChannels: React.FunctionComponent = () => {
+  const channelList = useSelector(getChannelList);
+  const channelListFetchStatus = useSelector((state: RidiSelectState) => state.articleChannels.fetchStatus);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (channelListFetchStatus === FetchStatusFlag.IDLE && channelList.length === 0) {
+      dispatch(Actions.loadArticleChannelListRequest());
+    }
+  }, []);
   return (
     <main
       className={classNames(
@@ -15,7 +28,19 @@ export const ArticleChannels: React.FunctionComponent = () => {
     >
       <HelmetWithTitle titleName={PageTitleText.ARTICLE_CHANNEL} />
       <div className="a11y"><h1>리디셀렉트 아티클 전체 채널</h1></div>
-      <ArticleChannelList />
+      <section>
+        <div className="ArticlePageChannelList_Wrap">
+          <ul className="ArticlePageChannelList">
+            {channelList.map((channelMeta, idx) => {
+                return channelMeta ? (
+                  <li key={idx} className="ArticlePageChannel">
+                    <ArticleChannelsMeta {...channelMeta} />
+                  </li>
+                ) : null;
+            })}
+          </ul>
+        </div>
+      </section>
     </main>
   );
 };
