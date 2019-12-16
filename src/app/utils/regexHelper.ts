1bc415f7623549cb86b92918b29395d5ad4bf69e
이준우 <junwoo.lee@ridi.com>
2019-12-16 12:43:39 +0900

    셀렉트 url 체크하는 로직 잘못된 부분 수정.

--- src/app/utils/regexHelper.ts
+++ src/app/utils/regexHelper.ts
@@ -3,7 +3,7 @@ import Env from 'app/config/env';
 export function isRidiselectUrl(url: string) {
   const schemeReg = /^https?:\/\//;
   const selectUrl = schemeReg.test(Env.SELECT_URL)
-    ? Env.SELECT_URL.replace('https', '')
+    ? Env.SELECT_URL.replace('https:', '')
     : Env.SELECT_URL;
   const ridiselectReg = new RegExp(`^https?:${selectUrl}`);
 
