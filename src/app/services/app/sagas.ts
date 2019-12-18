41fb6fafb04336acc882d9fcdbbe340ad8402489
Heejae Jang <j132134@gmail.com>
2019-12-18 18:45:46 +0900

    Simplify

--- src/app/services/app/sagas.ts
+++ src/app/services/app/sagas.ts
@@ -6,11 +6,13 @@ import { RidiSelectState } from 'app/store';
 const KEY_LOCAL_STORAGE = 'rs.home';
 export const localStorageManager = {
   load: (): AppStatus => {
-    const data: AppStatus = (localStorage.getItem(KEY_LOCAL_STORAGE) || AppStatus.Books) as AppStatus;
-    return Object.values(AppStatus).includes(data) ? data : AppStatus.Books;
+    const data = localStorage.getItem(KEY_LOCAL_STORAGE) as AppStatus;
+    return [AppStatus.Books, AppStatus.Articles].includes(data)
+      ? data
+      : AppStatus.Books;
   },
-  save: (value: AppStatus) => {
-    localStorage.setItem(KEY_LOCAL_STORAGE, value);
+  save: (status: AppStatus) => {
+    localStorage.setItem(KEY_LOCAL_STORAGE, status);
   },
 };
 
@@ -18,8 +20,6 @@ export function* watchAppState() {
   while (true) {
     yield take(Actions.updateAppStatus.getType());
     const appStatus = yield select((state: RidiSelectState) => state.app.appStatus);
-    if ([AppStatus.Books, AppStatus.Articles].includes(appStatus)) {
-      localStorageManager.save(appStatus);
-    }
+    localStorageManager.save(appStatus);
   }
 }
