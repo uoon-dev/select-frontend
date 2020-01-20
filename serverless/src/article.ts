72b4e788b46df58939e2107aebd265327c5f1e71
Heejae Jang <j132134@gmail.com>
2020-01-20 17:32:51 +0900

    Fix: replace html opening, closing tags in og:description

--- serverless/src/article.ts
+++ serverless/src/article.ts
@@ -1,12 +1,7 @@
 import Router from 'express-promise-router';
-
 import fetch from 'node-fetch';
-import ellipsis from 'text-ellipsis';
-import { AllHtmlEntities } from 'html-entities';
-
 import { override, OpenGraph } from './utils/override';
-
-const MAX_DESCRIPTION_LENGTH = 175;
+import { ellipsisDescription } from './utils/descriptions';
 
 const router = Router();
 
@@ -42,16 +37,9 @@ router.get('/channel/@:channelName', async (req, res) => {
   const { channelName } = req.params;
   try {
     const data = await (await fetch(getArticleApiUrl(channelName))).json();
-    const entities = new AllHtmlEntities();
-    const description = entities.decode(
-      ellipsis(
-        data.description.replace(/&(\w+);/, ''),
-        MAX_DESCRIPTION_LENGTH
-      )
-    );
     const openGraph: Partial<OpenGraph> = {
       title: `${data.display_name} - 리디셀렉트`,
-      description,
+      description: ellipsisDescription(data.description),
       type: 'article',
       url: `https://select.ridibooks.com/article/channel/@${channelName}`,
       image: data.thumbnail_url,
