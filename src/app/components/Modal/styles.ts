073e89bc4dc28f0d4322588bce4723120d1c4b9b
이준우 <junwoo.lee@ridi.com>
2020-01-22 15:00:55 +0900

    modal component 작성.

---
+++ src/app/components/Modal/styles.ts
@@ -0,0 +1,69 @@
+import { css } from '@emotion/core';
+
+import Colors from 'app/styles/colors';
+import { resetButton } from 'app/styles/customProperties';
+
+export const modalContainer = css`
+  position: fixed;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+
+  &:after {
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+    content: '';
+    background: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+export const modalComponent = css`
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  background: #FFFFFF;
+  border-radius: 3px;
+  z-index: 20;
+`;
+
+export const modalHeader = css`
+  display: block;
+  position: relative;
+  padding: 20px 54px 20px 20px;
+  box-sizing: border-box;
+`;
+
+export const modalTitle = css`
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 24px;
+  color: #000;
+`;
+
+export const modalCloseButton = css`
+  ${resetButton}
+  width: 54px;
+  height: 64px;
+`;
+
+export const modalContent = css`
+  display: block;
+  padding: 0 20px 20px;
+  box-sizing: border-box;
+`;
+
+export const closeButtonIcon = css`
+  width: 24px;
+  height: 24px;
+  fill: ${Colors.slategray_50};
+`;
