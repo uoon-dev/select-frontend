42bf45e92be091e5914a2f8c81d390d23636dd33
이준우 <junwoo.lee@ridi.com>
2020-01-29 14:44:00 +0900

    불필요하게 길게 사용하고 있는 지역변수명 간결하게 수정.

--- src/app/components/OrderHistory/CashReceiptIssueModal.tsx
+++ src/app/components/OrderHistory/CashReceiptIssueModal.tsx
@@ -19,17 +19,17 @@ export const CashReceiptIssueModal: React.FunctionComponent<CashReceiptIssueModa
 
   const isCashReceiptIssueFetching = useSelector((state: RidiSelectState) => state.user.purchaseHistory.isCashReceiptIssueFetching);
 
-  const [cashReceiptIssueType, setCashReceiptIssueType] = React.useState('INCOME_DEDUCTION')
-  const [cashReceiptIssueModalIssueNumber, setCashReceiptIssueModalIssueNumber] = React.useState('');
+  const [IssueType, setIssueType] = React.useState('INCOME_DEDUCTION')
+  const [IssueNumber, setIssueNumber] = React.useState('');
 
   const dispatch = useDispatch();
 
-  const handleCashReceiptIssueTypeChange = (e: any) => {
+  const handleIssueTypeChange = (e: any) => {
     if (!e.currentTarget.checked) {
       return;
     }
-    setCashReceiptIssueModalIssueNumber('');
-    setCashReceiptIssueType(e.currentTarget.value);
+    setIssueNumber('');
+    setIssueType(e.currentTarget.value);
   }
 
   const submitCashReceiptIssueRequest = () => {
@@ -37,7 +37,7 @@ export const CashReceiptIssueModal: React.FunctionComponent<CashReceiptIssueModa
       return;
     }
 
-    if (!cashReceiptIssueModalIssueNumber || cashReceiptIssueModalIssueNumber.length <= 0) {
+    if (!IssueNumber || IssueNumber.length <= 0) {
       toast.failureMessage('번호를 확인해주세요.');
       return;
     }
@@ -45,8 +45,8 @@ export const CashReceiptIssueModal: React.FunctionComponent<CashReceiptIssueModa
     dispatch(Actions.cashReceiptIssueRequest({
       ticketId: id,
       method: 'POST',
-      issuePurpose: cashReceiptIssueType,
-      issueNumber: cashReceiptIssueModalIssueNumber,
+      issuePurpose: IssueType,
+      issueNumber: IssueNumber,
     }));
   }
 
@@ -60,21 +60,21 @@ export const CashReceiptIssueModal: React.FunctionComponent<CashReceiptIssueModa
         <ul css={styles.cashReceiptIssueModalIssueTypeList}>
           <li css={styles.cashReceiptIssueModalIssueTypeListItem}>
             <Radio
-              inputName="cashReceiptIssueType"
+              inputName="IssueType"
               id="cashReceipt_incomeDeduction"
               value="INCOME_DEDUCTION"
-              isChecked={cashReceiptIssueType === 'INCOME_DEDUCTION'}
-              onChange={handleCashReceiptIssueTypeChange}
+              isChecked={IssueType === 'INCOME_DEDUCTION'}
+              onChange={handleIssueTypeChange}
               displayName="소득 공제용"
             />
           </li>
           <li css={styles.cashReceiptIssueModalIssueTypeListItem}>
             <Radio
-              inputName="cashReceiptIssueType"
+              inputName="IssueType"
               id="cashReceipt_ExpenseEvidence"
               value="EXPENSE_EVIDENCE"
-              isChecked={cashReceiptIssueType === 'EXPENSE_EVIDENCE'}
-              onChange={handleCashReceiptIssueTypeChange}
+              isChecked={IssueType === 'EXPENSE_EVIDENCE'}
+              onChange={handleIssueTypeChange}
               displayName="지출 증빙용"
             />
           </li>
@@ -84,9 +84,9 @@ export const CashReceiptIssueModal: React.FunctionComponent<CashReceiptIssueModa
           <input
             type="text"
             css={styles.cashReceiptIssueModalIssueNumberInput}
-            placeholder={cashReceiptIssueType === 'INCOME_DEDUCTION' ? '주민등록번호 또는 휴대폰 번호 입력' : '사업자 번호 입력'}
-            onChange={(e) => setCashReceiptIssueModalIssueNumber(e.currentTarget.value)}
-            value={cashReceiptIssueModalIssueNumber}
+            placeholder={IssueType === 'INCOME_DEDUCTION' ? '주민등록번호 또는 휴대폰 번호 입력' : '사업자 번호 입력'}
+            onChange={(e) => setIssueNumber(e.currentTarget.value)}
+            value={IssueNumber}
           />
         </div>
         <div css={styles.cashReceiptIssueModalIssueButtonWrapper}>
