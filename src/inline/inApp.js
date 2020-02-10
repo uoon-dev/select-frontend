8070536cb75f393b7790cdc4b2102f4e14f11d11
이준우 <junwoo.lee@ridi.com>
2020-02-10 17:26:01 +0900

    remove js filter from lint rule.

--- src/inline/inApp.js
+++ src/inline/inApp.js
@@ -5,7 +5,7 @@ function isExternalLink(url) {
   return new URL(url, location.href).hostname !== location.hostname;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
   if (/ridibooks\/[0-9]+\.?[0-9]*/i.test(window.navigator.userAgent)) {
     if (/iphone|ipad|ipod/i.test(window.navigator.userAgent)) {
       document.body.classList.add('iosApp');
