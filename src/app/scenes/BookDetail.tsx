8eb61d61b46bfe6efc8f450ddf98121d04c29270
JunWoo <junu.sinclair@gmail.com>
2020-03-04 23:48:33 +0900

    도서 상세페이지에서 실패한 응답에 대해 반복적으로 재요청 하는 것 수정.

--- src/app/scenes/BookDetail.tsx
+++ src/app/scenes/BookDetail.tsx
@@ -54,13 +54,13 @@ export class BookDetail extends React.Component<Props> {
     }
     if (
       props.isLoggedIn &&
-      props.ownershipFetchStatus !== FetchStatusFlag.FETCHING &&
+      props.ownershipFetchStatus === FetchStatusFlag.IDLE &&
       !props.ownershipStatus
     ) {
       props.dispatchLoadBookOwnershipRequest(props.bookId);
     }
     if (
-      props.bookToBookRecommendationFetchStatus !== FetchStatusFlag.FETCHING &&
+      props.bookToBookRecommendationFetchStatus === FetchStatusFlag.IDLE &&
       !props.recommendedBooks
     ) {
       props.dispatchLoadBookToBookRecommendation(props.bookId);
