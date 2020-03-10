3f1ec3c53ba9bb778bec1d9ec2c38ac16cfa9327
nnyamm <nnyamm@ridi.com>
2020-03-10 15:54:53 +0900

    MediaQuery 를 state 로 변경

--- src/app/components/ExpandableBookList.tsx
+++ src/app/components/ExpandableBookList.tsx
@@ -1,11 +1,11 @@
 import classNames from 'classnames';
 import React from 'react';
-import MediaQuery from 'react-responsive';
+import { useSelector } from 'react-redux';
 
 import { ConnectedInlineHorizontalBookList } from 'app/components';
 import { Expander } from 'app/components/BookDetail/Expander';
-import { MAX_WIDTH } from 'app/constants';
 import { Book } from 'app/services/book';
+import { getIsMobile } from 'app/services/commonUI/selectors';
 
 interface ExpandableBookListProps {
   className: string;
@@ -18,6 +18,7 @@ interface ExpandableBookListProps {
 export const ExpandableBookList: React.FunctionComponent<ExpandableBookListProps> = props => {
   const [isExpanded, setIsExpanded] = React.useState(false);
   const { className, listTitle, uiPartTitleForTracking, books, pageTitleForTracking } = props;
+  const isMobile = useSelector(getIsMobile);
 
   return books && books.length > 0 ? (
     <section
@@ -28,31 +29,25 @@ export const ExpandableBookList: React.FunctionComponent<ExpandableBookListProps
       )}
     >
       <h2 className="ExpandableBookList_Title">{listTitle}</h2>
-      <MediaQuery maxWidth={MAX_WIDTH}>
-        {isMobile => (
-          <>
-            <ConnectedInlineHorizontalBookList
-              books={isExpanded || isMobile ? books : books.slice(0, 6)}
-              serviceTitleForTracking="select-book"
-              uiPartTitleForTracking={uiPartTitleForTracking}
-              disableInlineOnPC={isExpanded}
-              renderAuthor={false}
-              lazyloadThumbnail={false}
-              bookThumbnailSize={isMobile ? 110 : 120}
-              pageTitleForTracking={pageTitleForTracking}
-            />
-            {books.length > 6 && !isMobile && !isExpanded && (
-              <div className="ExpandableBookList_ExpandButton_Wrapper">
-                <Expander
-                  onClick={() => setIsExpanded(!isExpanded)}
-                  text={isExpanded ? '접기' : '펼쳐 보기'}
-                  isExpanded={isExpanded}
-                />
-              </div>
-            )}
-          </>
-        )}
-      </MediaQuery>
+      <ConnectedInlineHorizontalBookList
+        books={isExpanded || isMobile ? books : books.slice(0, 6)}
+        serviceTitleForTracking="select-book"
+        uiPartTitleForTracking={uiPartTitleForTracking}
+        disableInlineOnPC={isExpanded}
+        renderAuthor={false}
+        lazyloadThumbnail={false}
+        bookThumbnailSize={isMobile ? 110 : 120}
+        pageTitleForTracking={pageTitleForTracking}
+      />
+      {books.length > 6 && !isMobile && !isExpanded && (
+        <div className="ExpandableBookList_ExpandButton_Wrapper">
+          <Expander
+            onClick={() => setIsExpanded(!isExpanded)}
+            text={isExpanded ? '접기' : '펼쳐 보기'}
+            isExpanded={isExpanded}
+          />
+        </div>
+      )}
     </section>
   ) : null;
 };
