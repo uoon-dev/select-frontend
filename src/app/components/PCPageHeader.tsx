330d820581f8f6eb92f32325dd4340697963896e
nnyamm <nnyamm@ridi.com>
2020-03-17 16:17:57 +0900

    최대값이 상이한 부분 확인 및 useSelector 로 변경

--- src/app/components/PCPageHeader.tsx
+++ src/app/components/PCPageHeader.tsx
@@ -1,12 +1,14 @@
-import { MAX_WIDTH } from 'app/constants';
 import React from 'react';
-import MediaQuery from 'react-responsive';
+import { useSelector } from 'react-redux';
+
+import { getIsMobile } from 'app/services/commonUI/selectors';
 import { ConnectedPageHeader, PageHeaderOwnProps } from './PageHeader';
 
-export const PCPageHeader: React.SFC<PageHeaderOwnProps> = props => (
-  <MediaQuery maxWidth={MAX_WIDTH + 1}>
-    {isMobile =>
-      isMobile ? <h1 className="a11y">{props.pageTitle}</h1> : <ConnectedPageHeader {...props} />
-    }
-  </MediaQuery>
-);
+export const PCPageHeader: React.SFC<PageHeaderOwnProps> = props => {
+  const isMobile = useSelector(getIsMobile);
+  return isMobile ? (
+    <h1 className="a11y">{props.pageTitle}</h1>
+  ) : (
+    <ConnectedPageHeader {...props} />
+  );
+};
