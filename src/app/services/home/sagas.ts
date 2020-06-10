1bbd7a3c92215dbd99a481fed90ffee9d62f1772
shrimp1box <shrimp1box@gmail.com>
2020-06-10 13:13:33 +0900

    reducer 에 포함된 side effects 를 saga 로 이동

--- src/app/services/home/sagas.ts
+++ src/app/services/home/sagas.ts
@@ -10,6 +10,7 @@ import { RidiSelectState } from 'app/store';
 import showMessageForRequestError from 'app/utils/toastHelper';
 import { getIsIosInApp } from 'app/services/environment/selectors';
 import { ErrorStatus } from 'app/constants/index';
+import { isRidiselectUrl } from 'app/utils/regexHelper';
 
 export function* watchLoadHome() {
   while (true) {
@@ -42,11 +43,22 @@ export function* watchLoadHome() {
         }),
       );
       yield put(CollectionActions.updateCollections({ collections }));
+      const isIosInApp = getIsIosInApp(state);
+      const bigBannerList = isIosInApp
+        ? responseWithoutPopular.banners.filter(banner => isRidiselectUrl(banner.linkUrl))
+        : responseWithoutPopular.banners;
+      const collectionIdList = responseWithoutPopular.collections.map(
+        collection => collection.collectionId,
+      );
+      // 홈 화면에서 현재 셀렉트 백엔드에서 내려주는 Home API의 섹션 리스트를 이용하여 렌더링을 자동화 처리를 해둔 상황에서 중간에 static한 위치에
+      // 데이터팀의 인기 도서 API의 응답 데이터를 활용해야하는 상황이라서 collection id list의 3번째 포지션에 임의로 인기도서 섹션에 해당하는 id를 넣어주는 처리.
+      // TODO: 후에 홈 화면의 섹션 렌더링 방식을 개선하면서 이렇게 부분적으로 처리하는 것을 어떻게 줄일지 고민 필요.
+      collectionIdList.splice(2, 0, 0);
       yield put(
         Actions.loadHomeSuccess({
-          response: responseWithoutPopular,
           fetchedAt: Date.now(),
-          isIosInApp: getIsIosInApp(state),
+          bigBannerList,
+          collectionIdList,
         }),
       );
     } catch (e) {
